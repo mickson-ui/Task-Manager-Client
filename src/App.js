59ca3d6a0c98d62159cc378a38eaa8866b98eee0
Mickson Bonsu Bossman <mbonsu69@outlook.com>
2024-12-01 17:35:11 +0100

    First Task manager app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    fetch('/')
+      .then((response) => response.text())
+      .then((data) => setMessage(data))
+      .catch((error) => console.error('Error fetching data:', error))
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Task Manager App</h1>
+      <p>{ message }</p>
     </div>
   );
 }
